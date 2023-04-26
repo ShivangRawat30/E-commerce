587326a08b746e2434864e136bb329306403e3da
Shivang Rawat <rawatshivang30@gmail.com>
2023-04-26 22:56:52 +0530

    mongoconnection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 
 // Config
 dotenv.config({ path: "backend/config/config.env" });
+
+//Connecting Database
+connectDatabase();
 app.listen(process.env.PORT, () => {
   console.log(
     `Server is Working on Port http://localhost:${process.env.PORT} `
